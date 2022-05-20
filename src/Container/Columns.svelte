08b9bac9a114f709cbc57b72b9b15429115abba7
hvdev2k1 <104979538+hvdev2k1@users.noreply.github.com>
2022-05-20 21:38:58 +0700

    feat(component): flexbox-based container

---
+++ src/Container/Columns.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let reverse = false;
+</script>
+
+<div class="container">
+    <div class:reverse class="columns">
+        <slot />
+    </div>
+</div>
+
+<style>
+    .container {
+        flex: 1 1 0;
+    }
+
+    .columns {
+        display: flex;
+    }
+
+    .reverse {
+        flex-direction: row-reverse;
+    }
+</style>
