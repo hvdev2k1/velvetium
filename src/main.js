40819989a210a2814d50e515077406c918e1b02a
hvdev2k1 <104979538+hvdev2k1@users.noreply.github.com>
2022-05-21 11:19:26 +0700

    refactor(component): seperate Container component

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 export { default as Columns } from './Container/Columns.svelte'
 export { default as Rows } from './Container/Rows.svelte'
+export { default as Container } from './Container/Container.svelte'
