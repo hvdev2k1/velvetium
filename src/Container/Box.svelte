dca9d421e4e892ee3d3d431038d53ecff4f2e926
hvdev2k1 <hvhvdevdev@pm.me>
2022-05-28 08:44:29 +0000

    impl(component): Box

---
+++ src/Container/Box.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Container from "./Container.svelte";
+</script>
+
+<Container>
+    <div>
+        <slot />
+    </div>
+</Container>
+
+<style>
+    div {
+        border: 1px solid rgba(0, 0, 0, 0.02);
+        background-color: rgba(0, 0, 0, 0.02);
+        padding: 5px;
+        border-radius: 5px 5px 5px 5px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 export { default as Columns } from './Container/Columns.svelte'
 export { default as Rows } from './Container/Rows.svelte'
 export { default as Container } from './Container/Container.svelte'
+export { default as Box } from './Container/Box.svelte'
