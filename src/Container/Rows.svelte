29d8edef9adcc3509f04d124a74ef6ef9688e921
hvdev2k1 <hvhvdevdev@pm.me>
2022-06-01 14:36:48 +0000

    feat(component): columns/rows gaps

--- src/Container/Rows.svelte
+++ src/Container/Rows.svelte
@@ -2,10 +2,11 @@
     import Container from "./Container.svelte";
 
     export let reverse = false;
+    export let gapless = false;
 </script>
 
 <Container>
-    <div class:reverse>
+    <div class:reverse class:gap={!gapless}>
         <slot />
     </div>
 </Container>
@@ -14,6 +15,19 @@
     div {
         display: flex;
         flex-direction: column;
+    }
+
+    div.gap > :global(*) {
+        box-sizing: border-box;
+        margin: 0.75rem 0;
+    }
+
+    div.gap > :global(:first-child) {
+        margin-top: 0;
+    }
+
+    div.gap > :global(:last-child) {
+        margin-bottom: 0;
     }
 
     .reverse {
